1b96f8133b4de8b2cb32c335926440b28396a3c0
vineetsouri <vineetsouri72@gmail.com>
2016-06-20 17:03:17 +0530

    added node modules and settign up basic skeleton for REACT(in complete)

---
+++ src/components/List.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredients = [{"id":1,"text":"web designer"},{"id":2,"text":"web developer"},{"id":3,"text":"Tester"}];
+
+var List = React.
